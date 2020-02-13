a617e95cc4781bcb4abaf200905f048eb8ed8353
Jasmine <jasmine.pilkerton.2022@student.lhschools.org>
2020-02-13 11:12:23 -0500

    called hello world method

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,12 +1,14 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        helloWorld();
 
         
     }
 
     public static void helloWorld(){
-        System.out.println("Hello World");
+
+        System.out.println("Hello github World");
     }
 
 
